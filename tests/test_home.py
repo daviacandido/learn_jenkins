2905f12a97b50cbe2e5490a3dbf9596feb56f016
daviacandido <daviaraujocandido@gmail.com>
2025-04-04 10:32:09 -0300

    edit test_home.py

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,6 +4,6 @@ def test_home(browser):
     url='http://127.0.0.1:8080'
     browser.get(url)
     time.sleep(10)
-    assert browser.title == 'Dashboard'
+    assert browser.title == 'Dash'
 
     print('Teste da página inicial concluído com sucesso.')
